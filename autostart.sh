99184f80b4a994134faa8545d36ec57b053acefb
RaquVaza <gametwodays228@gmail.com>
2026-04-14 13:46:02 +0500

    gaps patch

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,5 @@
 xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rate 144 --rotate normal --output DP-4 --primary --mode 2560x1440 --pos 2560x0 --rate 165 --rotate normal &
 nm-applet &
 blueman-applet &
-feh --bg-scale Pictures/Wallpapers/jesus.jpg &
-picom &
+feh --bg-scale /home/zer0day/Pictures/Wallpapers/jesus.jpg &
 
--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Monocraft:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
@@ -18,11 +18,8 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-static const unsigned int gappx = 8; //общий отступ
-static const unsigned int gappih = 8;//внутренний горизонтальный
-static const unsigned int gappiv = 8;//внутренний вертикальный
-static const unsigned int gappoh = 8;//внешний горизонтальный
-static const unsigned int gappov = 8;//внешний вертикальный
+static const unsigned int gappx = 8;
+static const unsigned int gappi = 8;
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
