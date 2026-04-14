54789b163ab007c61f791bbc5d2b4c575af04582
RaquVaza <gametwodays228@gmail.com>
2026-04-14 20:16:27 +0500

    prepatches

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rate 144 --rotate normal --output DP-4 --primary --mode 2560x1440 --pos 2560x0 --rate 165 --rotate normal &
+
+sleep 2 &
 nm-applet &
 blueman-applet &
 feh --bg-scale /home/zer0day/Pictures/Wallpapers/jesus.jpg &
--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Monocraft:size=10" };
@@ -69,29 +69,16 @@ static const char *rofi[] = { "rofi", "-show", "drun", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	/*    launch	*/
 	{ MODKEY,	                XK_Return, spawn,          {.v = term } },
 	{ MODKEY,                       XK_b,      spawn,          {.v = web} },
 	{ MODKEY,			XK_Tab,	   spawn,	   {.v = rofi} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,			XK_e,	   spawn,	   {.v = filemanager} },
+
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY|ShiftMask,             XK_Tab,    view,           {0} },
 	{ MODKEY,	                XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-//	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY,             		XK_f,	   togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
