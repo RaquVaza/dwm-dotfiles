d97a80888ab88d41a82f29fec16fdd2300a90481
RaquVaza <gametwodays228@gmail.com>
2026-04-14 12:49:27 +0500

    add to github

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rate 144 --rotate normal --output DP-4 --primary --mode 2560x1440 --pos 2560x0 --rate 165 --rotate normal &
+nm-applet &
+blueman-applet &
+feh --bg-scale Pictures/Wallpapers/jesus.jpg &
+picom &
+
